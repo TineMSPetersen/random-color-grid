4b1d826f02fb9e0ed8b78e5d37797e5c60a02a2a
Eyenoom <tinemeretesikjaerpetersen@gmail.com>
2022-08-26 23:34:12 +0200

    Select elements and add event listener

---
+++ script.js
@@ -0,0 +1,7 @@
+// Selects all the squares
+const elements = document.querySelectorAll(".element");
+console.log(elements);
+
+
+// Adds event listener to activate when the square is clicked
+elements.addEventListener('click, changeColor()');
